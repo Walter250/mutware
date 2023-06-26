06efac13f0fdd981a3118d6e1422ceb744451d0a
Walter250 <turikumwew@gmail.com>
2023-06-27 01:33:56 +0200

    Boilerplate

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+    <title>Tailwind CSS Test</title>
+</head>
+<body>
+    <div style="width: 1110px; height: 61px; padding-top: 10px; padding-bottom: 9px; padding-left: 327.50px; justify-content: flex-end; align-items: center; gap: 241.50px; display: inline-flex">
+        <div style="justify-content: flex-start; align-items: flex-end; gap: 60px; display: inline-flex">
+          <div style="text-align: right; color: #1A4072; font-size: 14px; font-family: Poppins; font-weight: 500; word-wrap: break-word">Courses</div>
+          <div style="text-align: right; color: #1A4072; font-size: 14px; font-family: Poppins; font-weight: 500; word-wrap: break-word">Career</div>
+          <div style="text-align: right; color: #1A4072; font-size: 14px; font-family: Poppins; font-weight: 500; word-wrap: break-word">Forum</div>
+          <div style="text-align: right; color: #1A4072; font-size: 14px; font-family: Poppins; font-weight: 500; word-wrap: break-word">Contact us</div>
+        </div>
+        <div class="w-[130px] h-[42px] relative">
+            <div class="w-[130px] h-[42px] bg-sky-900 rounded-3xl border border border border border-stone-500">Login</div>
+        </div>
+    </div>
+</body>
+</html>
